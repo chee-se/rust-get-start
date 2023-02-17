f4efc162c97fac42e7565ca898d04c57a491a4a8
Yukihiro Chiba <chiba.yukihiro.biz@gmail.com>
2023-02-17 16:39:05 +0000

    chore: set locale ja_JP

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,11 +1,18 @@
 apt-get update
 apt-get -y install git
 apt-get -y install curl
+apt-get -y install locales
+
+echo 'ja_JP UTF-8' >> /etc/locale.gen
+locale-gen
 
 curl -o ~/.git-completion.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
 echo "
+
+# set locale
+LANG='ja_JP.utf8'
 
 # git bash settings
 
@@ -23,4 +30,6 @@ fi
 #GIT_PS1_SHOWUPSTREAM=auto
 
 PS1='[\\[\\033[32m\\]\\u@\\h\\[\\033[00m\\] \\[\\033[33m\\]\\w\\[\\033[1;36m\\]\$(\\_\\_git_ps1 \" (%s)\")\\[\\033[00m\\]]\\n\\$ '
+
+GIT_PAGER='LESSCHARSET=utf-8 less'
 " >> ~/.bashrc
